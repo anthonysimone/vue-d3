89a6f1f25cd54f1fd361927ce0f2d33496be1807
Anthony Simone <anthony.simone7@gmail.com>
2019-03-30 10:09:52 -0700

    Save progress

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,10 @@
     <page-header pageTitle="Visualize Your Data" subTitle="A tool to help you get started visualizing your data on the web with D3"></page-header>
     <div class="container">
       <section class="section">
-        <img alt="Vue logo" src="../assets/logo.png">
+        <div class="content">
+          <h2 class="title is-2">Welcome to D3 Helper!</h2>
+          <p></p>
+        </div>
       </section>
     </div>
   </div>
